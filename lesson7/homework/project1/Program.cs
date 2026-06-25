14a35600017798369ca1978b93f7e15b25adec9a
frienddrive6-afk <frienddrive6@gmail.com>
2026-06-25 20:09:47 +0300

    add homework

---
+++ lesson7/homework/project1/Program.cs
@@ -0,0 +1,30 @@
+﻿using project1;
+
+
+SuperParallel.Invoke(() => 
+{
+    Console.WriteLine($"Task 1 started on Thread: {Thread.CurrentThread.ManagedThreadId}");
+    Thread.Sleep(1000);
+    Console.WriteLine("Task 1 finished");
+},() => 
+{
+Console.WriteLine($"Task 2 started on Thread: {Thread.CurrentThread.ManagedThreadId}");
+Thread.Sleep(500);
+Console.WriteLine("Task 2 finished");
+});
+
+
+SuperParallel.For(0, 5, (index) => {
+    Console.WriteLine($"Iteration {index} executed on Thread: {Thread.CurrentThread.ManagedThreadId}");
+    Thread.Sleep(300);
+});
+
+
+string[] names = { "Alice", "Bob", "Charlie", "Diana" };
+        
+SuperParallel.ForEach(names, (name) =>
+{
+    Console.WriteLine($"Hello, {name}! (Processed on Thread: {Thread.CurrentThread.ManagedThreadId})");
+    Thread.Sleep(400);
+        
+});
---
+++ lesson7/homework/project1/SuperParallel.cs
@@ -0,0 +1,54 @@
+namespace project1;
+
+public static class SuperParallel
+{
+    public static void Invoke(params Action[] actions)
+    {
+        if (actions == null || actions.Length == 0) return;
+
+        Task[] tasks = new Task[actions.Length];
+
+        for (int i = 0; i < actions.Length; i++)
+        {
+            Action action = actions[i];
+            tasks[i] = Task.Run(action);
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    // 2. Метод For: параллельный аналог цикла for
+    public static void For(int fromInclusive, int toExclusive, Action<int> body)
+    {
+        if (body == null) return;
+        int iterationsCount = toExclusive - fromInclusive;
+        if (iterationsCount <= 0) return;
+
+        Task[] tasks = new Task[iterationsCount];
+
+        for (int i = fromInclusive; i < toExclusive; i++)
+        {
+            int index = i; 
+            
+            tasks[i - fromInclusive] = Task.Run(() => body(index));
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    public static void ForEach<T>(IEnumerable<T> source, Action<T> body)
+    {
+        if (source == null || body == null) return;
+
+        List<Task> tasks = new List<Task>();
+
+        foreach (T item in source)
+        {
+            T localItem = item;
+            
+            tasks.Add(Task.Run(() => body(localItem)));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+    }
+}
